2610a7a2b24d66edc8a61ec9f3ec19edeaf7a880
Leandro Ferreira <leandro.ferreira@gmail.com>
2024-03-18 16:23:50 -0300

    Adding infos after the live demo

--- lab1_otel_autointrumentation /run.sh
+++ lab1_otel_autointrumentation /run.sh
@@ -1,4 +1,11 @@
 
 
+#ENV VARIABLES
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
+export OTEL_RESOURCE_ATTRIBUTES="service.name=demoservice,service.namespace=otel-demo1"
+
+#APM AGENT VARIABLES
+export JAVA_TOOL_OPTIONS="-javaagent:opentelemetry-javaagent.jar"
+
 java -jar banking-2.1.0.jar
 
